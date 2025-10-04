69049ea39b39ccab122b4c3196887052319d4294
Aman1184 <amanpandey94158@gmail.com>
2025-10-04 17:49:59 +0530

    Integrate Cloudinary for image uploads to fix Render file system issues

--- backend/src/routes/upload.js
+++ backend/src/routes/upload.js
@@ -1,13 +1,17 @@
 const Image = require('../models/Image');
-const mkdirp = require('mkdirp');
-const sharp = require('sharp');
-const store = require('../store');
+const cloudinary = require('cloudinary').v2;
 const randomstring = require('randomstring');
+const store = require('../store');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 module.exports = async (req, res) => {
   const image = req.files.image;
   const { crop } = req.fields;
-  const path = image.path;
 
   if (!image) {
     return res.status(500).json({ status: 500, error: 'FILE_REQUIRED' });
@@ -15,9 +19,7 @@ module.exports = async (req, res) => {
 
   const shield = randomstring.generate({ length: 32, charset: 'alphanumeric', capitalization: 'lowercase' });
 
-  let imageObject;
-
-  imageObject = new Image({
+  let imageObject = new Image({
     name: image.name,
     author: req.user.id,
     size: image.size,
@@ -26,39 +28,27 @@ module.exports = async (req, res) => {
 
   await imageObject.save();
 
-  const folder = `${store.config.dataFolder}/${req.user.id}`;
-
-  try {
-    await mkdirp(folder);
-  } catch (err) {
-    return res.status(500).json({ status: 500, error: 'WRITE_ERROR' });
-  }
-
   const shieldedID = shield + imageObject._id;
 
-  const location = `${folder}/${shieldedID}.jpg`;
-
-  await sharp(path).rotate().toFile(location);
-
-  for (let i = 0; i < store.config.sizes.length; i++) {
-    const location = `${folder}/${shieldedID}-${store.config.sizes[i]}.jpg`;
-
-    let size = {};
-
-    if (crop === 'square') size = { width: store.config.sizes[i], height: store.config.sizes[i] };
-    else size = { width: store.config.sizes[i] };
-
-    await sharp(path).rotate().resize(size).toFile(location);
-  }
-
-  imageObject.location = location;
-  imageObject.shieldedID = shieldedID;
-
   try {
-    await imageObject.save();
-  } catch (err) {
-    res.status(500).json({ status: 500, error: 'DATABASE_ERROR' });
-  }
+    // Upload original image to Cloudinary
+    const result = await cloudinary.uploader.upload(image.path, {
+      folder: `clover/${req.user.id}`,
+      public_id: shieldedID,
+      resource_type: 'image',
+    });
 
-  res.status(200).json({ status: 200, image: imageObject });
+    imageObject.location = result.secure_url;
+    imageObject.shieldedID = shieldedID;
+
+    // Optionally, create resized versions (thumbnails) using Cloudinary transformations
+    // You can generate URLs with transformations on the frontend as needed
+
+    await imageObject.save();
+
+    res.status(200).json({ status: 200, image: imageObject });
+  } catch (err) {
+    console.error('Cloudinary upload error:', err);
+    res.status(500).json({ status: 500, error: 'UPLOAD_ERROR' });
+  }
 };
